ac64bf2b2d7cd674eaff0f1ccb4c6d8cdb16c91d
Vishal Rana <vishal.ch.36@gmail.com>
2021-01-10 01:36:19 +0530

    npm package added

--- assets/js/attendance_report.js
+++ assets/js/attendance_report.js
@@ -1,7 +1,46 @@
 import {SITE_ROOT} from './init.js';
 
 window.onload=()=>{
-  $.get(SITE_ROOT+'api/getEmpReports',function(res){
+  $.get(SITE_ROOT+'api/getEmpReports',(res)=>{
+      renderTable(res);
+  });
+
+}
+
+
+document.querySelector('#from').addEventListener('change',function(){
+  let from = this.value;
+  let to = document.querySelector('#to').value;
+  let payload ={
+    'from':from,
+    'to':to
+  } ;
+  $.post(SITE_ROOT+'api/getEmpReportsDateRange/',payload,(res)=>{
+    renderTable(res);
+  });
+});
+document.querySelector('#to').addEventListener('change',function(){
+  let from = document.querySelector('#from').value;
+  let to = this.value;
+  let payload ={
+    'from':from,
+    'to':to
+  } ;
+  $.post(SITE_ROOT+'api/getEmpReportsDateRange/',payload,(res)=>{
+    renderTable(res);
+  });
+});
+function renderTable(res){
+  let thead = document.createElement('thead');
+    thead.classList='bg-dark text-white';
+    let headers = ['Emp Id','Name','Location','Shift','Department','Mobile','Date','In Time','Out Time','Total Time Spent'];
+    headers.forEach(header => {
+      let col = document.createElement('td');
+      col.setAttribute('scope','col');
+      col.innerHTML= header;
+      thead.append(col);
+    })
+    $('#emp_report').html(thead);
     res.forEach(tran => {
       let row = document.createElement('tr');
       row.setAttribute('scope',"row");
@@ -12,6 +51,4 @@ window.onload=()=>{
       }
       $('#emp_report').append(row);
     });
-  });
-
 }
